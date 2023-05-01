900e3a9fac51104f69fe1b0c148575301ab8ddf3
Chung <tcyeung1128@gmail.com>
2023-05-02 03:47:13 +0800

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './Pages/login/login.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { FormsModule } from '@angular/forms';
-import { SignupComponent } from './signup/signup.component';
+import { SignupComponent } from './Pages/signup/signup.component';
 
 
 @NgModule({
